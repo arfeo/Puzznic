2a05843a17edd9915da37c326c9f2821e24eb13a
Leonid Belikov <mail@arfeo.net>
2019-01-20 22:33:32 +0300

    small upd

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,21 +3,21 @@ import { APP } from '../constants/global';
 /**
  * Function calculates the analogue of CSS vmin in pixels
  */
-const calculateVMin = (): number => {
+function calculateVMin(): number {
   const vpWidth: number = window.innerWidth;
   const vpHeight: number = window.innerHeight;
 
   return vpWidth >= vpHeight ? (vpHeight / 100) : (vpWidth / 100);
-};
+}
 
 /**
  * Function returns the cell size (atomic canvas measure)
  * depending on the screen size if no cell size is given in the app settings,
  * or cell size given in the app settings
  */
-const setCellSize = (): number => {
+function setCellSize(): number {
   return APP.cellSize > 0 ? APP.cellSize : Math.round(calculateVMin() * 6  / 10) * 10;
-};
+}
 
 /**
  * Function returns a random number in a given interval;
@@ -27,7 +27,7 @@ const setCellSize = (): number => {
  * @param max
  * @param discard
  */
-const getRandomNum = (min = 1, max = 1, discard: number[] = []): number => {
+function getRandomNum(min = 1, max = 1, discard: number[] = []): number {
   const num: number = Math.floor(min + Math.random() * (max + 1 - min));
 
   if (discard.indexOf(num) > -1) {
@@ -35,6 +35,6 @@ const getRandomNum = (min = 1, max = 1, discard: number[] = []): number => {
   }
 
   return num;
-};
+}
 
 export { setCellSize, getRandomNum };

d717a4d73f0bc6cb5fef9ca62fe7c3e97163775f
Leonid Belikov <mail@arfeo.net>
2020-05-05 06:51:28 +0300

    game constants upd

--- src/constants/game.ts
+++ src/constants/game.ts
@@ -18,7 +18,7 @@ export const TARGET_BLINK_DELAY = 300;
 
 export const BLOCK_FALL_SPEED = 10;
 
-export const ELEMENTS_COLORS: { [key: string]: { [key: string]: string } } = {
+export const ELEMENTS_COLORS: Record<'icon' | 'target', Record<string, string>> = {
   icon: {
     color1: 'rgb(255, 255, 255)',
     color2: 'rgb(0, 0, 0)',
@@ -29,4 +29,4 @@ export const ELEMENTS_COLORS: { [key: string]: { [key: string]: string } } = {
     state2: 'rgb(96, 95, 96)',
     state3: 'rgb(255, 255, 255)',
   },
-};
+} as const;

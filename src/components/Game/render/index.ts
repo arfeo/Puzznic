c2b09963383c96c512eb5ee744bbae45ffd7abb0
Leonid Belikov <mail@arfeo.net>
2020-05-09 14:46:38 +0300

    game render upd

--- src/components/Game/render/index.ts
+++ src/components/Game/render/index.ts
@@ -1,6 +1,5 @@
 export { renderBackground } from './background';
 export { renderBlock } from './block';
-export { renderBlockIcon } from './blockIcon';
 export { renderElementsList } from './elementsList';
 export { renderGameWindow } from './gameWindow';
 export { renderMap } from './map';
